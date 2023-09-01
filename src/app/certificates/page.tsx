9234a941d84c78d64bab0e9571ce85f797071070
alex8023 <alex8023@yandex.ru>
2023-09-01 16:25:10 +0300

    add Header / page & /certificates

---
+++ src/app/certificates/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import st from './certificates.module.css';
+// import { Layout } from '../Layout';
+import nodeJsImg from '@/img/CertSBNodeJS.png';
+import jsAdvImg from '@/img/CertSBJSAdvanced.png';
+import reactJsImg from '@/img/CertSBReactJS.png';
+import webLayoutImg from '@/img/CertSBWebLayout.png';
+import Paper from '@mui/material/Paper';
+import Image from 'next/image';
+
+export default function Certificates() {
+  return (
+    // <Layout>
+    <ul className={st.ul}>
+      {[reactJsImg, jsAdvImg, nodeJsImg, webLayoutImg].map((cert, i) => (
+        <li key={i} className={st.li}>
+          {/* <Paper elevation={7} sx={{ borderRadius: 5 }}> */}
+          <Image className={st.img} src={cert} alt='' />
+          {/* </Paper> */}
+        </li>
+      ))}
+    </ul>
+    // </Layout>
+  );
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,128 @@
+'use client';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import styles from './header.module.css';
+import Link from 'next/link';
+
+// interface Props {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window?: () => Window;
+// }
+
+const drawerWidth = 240;
+const navItems = [
+  { name: 'Portfolio', href: '/' },
+  { name: 'Certificates', href: '/certificates' },
+  // { name: 'Contacts', href: '/contacts' },
+  // { name: 'Выйти', href: '/login' },
+];
+
+export function Header() {
+  // const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        GrachevAI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <Link className={styles.navBtn} href={item.href}>
+                <ListItemText primary={item.name} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component='nav' sx={{ flexDirection: 'row', justifyContent: 'center' }}>
+        <Toolbar
+          sx={{
+            width: '1440px',
+            // height: '100px',
+            justifyContent: 'space-between',
+          }}
+        >
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant='h3'
+            component='div'
+            sx={{
+              flexGrow: 1,
+              // display: 'block',
+              display: { xs: 'none', sm: 'block' },
+            }}
+          >
+            GrachevAI
+          </Typography>
+
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Link key={item.name} className={styles.navBtn} href={item.href}>
+                <Button sx={{ color: '#fff' }} size='large'>
+                  {item.name}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component='nav'>
+        <Drawer
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
