6a1705eb6ef48741d67fcbf3208beea52f3e607f
alex8023 <alex8023@yandex.ru>
2023-09-03 18:14:52 +0300

    add mongodb

--- src/app/certificates/page.tsx
+++ src/app/certificates/page.tsx
@@ -8,6 +8,38 @@ import webLayoutImg from '@/img/CertSBWebLayout.png';
 import Paper from '@mui/material/Paper';
 import Image from 'next/image';
 import { Header } from '@/components/Header';
+import { GetServerSideProps } from 'next/types';
+import clientPromise from '@/lib/mongodb';
+
+type ConnectionStatus = {
+  isConnected: boolean;
+};
+let title: string = '';
+let description: string = '';
+(async () => {
+  try {
+    await clientPromise;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    const client = await clientPromise;
+    const db = client.db('grachev-ai');
+    const seoDB = db.collection('seo');
+    const seoData = await seoDB
+      .find({
+        page: 'certificates',
+      })
+      .toArray();
+    console.log(seoData[0].title);
+    title = seoData[0].title;
+    description = seoData[0].description;
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+  } catch (e) {
+    console.error(e);
+  }
+})();
 
 export default function Certificates() {
   return (
@@ -22,6 +54,10 @@ export default function Certificates() {
           </li>
         ))}
       </ul>
+      <div>
+        1{title}
+        {description}
+      </div>
     </>
   );
 }
