bd2122cce2993b79ccb5776d170d8f883c06fd5d
grachevdev <grachevdev@MacBook-Pro-alex.local>
2025-09-18 16:52:45 +0300

    add privacy policy for diary app

---
+++ src/app/diary/privacy/page.tsx
@@ -0,0 +1,12 @@
+export default function ToDoooPrivacy() {
+  return (
+    <>
+      <h1>Privacy policy application diary</h1>
+
+      <p>
+        Application diary does not collect user data and does not share it with third
+        parties
+      </p>
+    </>
+  );
+}
